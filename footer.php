68cfc8f5ffa77b544e2f2d85e9b64e9502567a0a
akrigline <andrew@akrigline.com>
2015-01-29 16:06:51 -0500

    clean up and start

--- footer.php
+++ footer.php
@@ -8,29 +8,15 @@
  */
 ?>
 
-	
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="<?php bloginfo('template_directory'); ?>/js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
 
-    <script src="<?php bloginfo('template_directory'); ?>/js/vendor/bootstrap.min.js"></script>
-    <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/vendor/jquery.parallax.min.js"></script>
-    <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/vendor/jquery.scrollTo.min.js"></script>
-    <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/vendor/jquery.localScroll.min.js"></script>
-    <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/vendor/jquery.bxslider.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/vendor/bootstrap.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/vendor/jquery.scrollTo.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/vendor/jquery.localScroll.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/vendor/skrollr.js"></script>
 
-    <script src="<?php bloginfo('template_directory'); ?>/js/main.js"></script>
-
-    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-57266603-1', 'auto');
-      ga('send', 'pageview');
-
-    </script>
-    <?php wp_footer(); ?>
+<script src="<?php bloginfo('template_directory'); ?>/js/main.js"></script>
+<?php wp_footer(); ?>
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,13 +1,59 @@
 jQuery(function( $ ){
-  $('.top-nav').localScroll({
-    duration:500,
-    hash:true
-  });
+
+	$(window).resize(function () {
+	  var $spy = $('[data-spy="scroll"]').scrollspy('refresh')
+	})
+
+	$('.top-nav').localScroll({
+	duration:500,
+	hash:true,
+	offset: -50
+	});
 });
 
+var picHeight = 1954;
+var picWidth = 3000;
+var aspectRatio = picWidth / picHeight;
+var divWidth, divHeight;
 
-if($(window).width() < 768){
-  $(document).ready(function(){
-    $('.about-slider').bxSlider();
-  });
-}
+var backgroundSize = function() {
+	var windowWidth = $(window).width();
+	var windowHeight = $(window).height();
+	var windowAspect = windowWidth / windowHeight;
+	if (windowAspect > aspectRatio) {
+	    divHeight = windowWidth * picHeight / picWidth;
+	    $("#teamBack .main-bg").css({
+	        height: divHeight,
+	        width: windowWidth,
+	        "margin-left": "0",
+	        "margin-top": -(divHeight - windowHeight) / 2,
+	        "background-size": "100% auto"
+	    })
+	} else {
+	    divWidth = windowHeight * picWidth / picHeight;
+	    $("#teamBack .main-bg").css({
+	        height: windowHeight,
+	        width: divWidth,
+	        "margin-top": "0",
+	        "margin-left": -((divWidth - windowWidth) / 2),
+	        "background-size": "auto 100%"
+	    })
+	}
+};
+$(document).ready(backgroundSize);
+$(window).resize(backgroundSize);
+
+
+$(window).ready( function() {
+	var s = skrollr.init({
+		forceHeight: false
+	});
+});
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-57266603-1', 'auto');
+ga('send', 'pageview');
