d3059a517fa5e3a63e22365bed3b0f0a15e01309
akrigline <andrew@akrigline.com>
2015-01-29 23:35:10 -0500

    fix footer, work

--- footer.php
+++ footer.php
@@ -30,13 +30,13 @@
 </footer>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="<?php bloginfo('template_directory'); ?>js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
+<script>window.jQuery || document.write('<script src="<?php bloginfo('template_directory'); ?>/js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
 
-<script src="<?php bloginfo('template_directory'); ?>js/vendor/bootstrap.min.js"></script>
-<script src="<?php bloginfo('template_directory'); ?>js/vendor/jquery.scrollTo.min.js"></script>
-<script src="<?php bloginfo('template_directory'); ?>js/vendor/jquery.localScroll.min.js"></script>
-<script src="<?php bloginfo('template_directory'); ?>js/vendor/skrollr.js"></script>
-<script src="<?php bloginfo('template_directory'); ?>js/main.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/vendor/bootstrap.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/vendor/jquery.scrollTo.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/vendor/jquery.localScroll.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/vendor/skrollr.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/main.js"></script>
 </body>
 </html>
 
