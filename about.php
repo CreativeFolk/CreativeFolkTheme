d9be4521fd2748543e5879a9b492be2a37fc297a
akrigline <andrew@akrigline.com>
2015-01-29 22:51:17 -0500

    about is done I think

--- about.php
+++ about.php
@@ -6,68 +6,45 @@
  */
 ?>
 
-<figure class="mainheader">
-	<img src="<?php bloginfo('template_directory');?>/img/creative_folk_handtype.png" class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2" />
-	<figcaption id="about">
-		<h1>Meet The Team</h1>
-	</figcaption>
-</figure>
-
-<section class="aboutsection">
-	<div class="container">
-
-		<?php query_posts('category_name=about'); ?>
-		<?php if (have_posts()) : ?>
-
-			<ul class="list-unstyled hidden-xs">
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php 
-					$title = get_field('title');
-					?>
-
-					<li class="col-xs-12 col-sm-6 col-md-4 about-entry">
-						<div class="headshot">
-							<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" class="mugshot hidden-lg">
-						</div>
-
-						<h2><?php the_title(); ?></h2>
-						<h3><?php echo $title; ?></h3>
-						<p><?php the_content(); ?></p>
-					</li>
-
-				<?php endwhile; // end of the loop. ?>
-			</ul>
 
 
-			<ul class="list-unstyled visible-xs about-slider">
-				<?php while ( have_posts() ) : the_post(); ?>
+<div id="team"></div> <!-- Blank space to make room for the position:fixed #teamback -->
+<section id="teamBack">
+	<div class="teamHeader sectionHeader">
+		<h1 class="sr-only">Meet the Team</h1>
+		<h3>Five designers with a common interest</h3>
+		<p class="col-sm-10 col-md-8 col-lg-6 col-sm-offset-1 col-md-offset-2 col-lg-offset-3">We started Creative Folk with the intention of collaborating on design work outside of school. All of us are currently pursueing a B.F.A. in Graphic Design from the University of Akron. We wanted to get some real world experience, but we wanted to do it as a group, not as individuals.</p>
+	</div>
+	<figure>
+		<div class="main-bg">
+			<?php query_posts('category_name=about'); ?>
+				<?php if (have_posts()) : ?>
 
-					<?php 
-					$title = get_field('title');
-					$image = get_field('mugshot'); 
-					?>
+					<?php while ( have_posts()) : the_post(); ?>
+						
+						<?php 
+							$slug = $post->post_name;
+							$title = get_field('title');
+						?>
 
-					<li class="col-xs-12 about-entry">
-						<div class="headshot">
-							<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" class="mugshot">
-						</div>
+						<div class="hoverTrigger <?php echo $slug; ?>"></div>
+						<figcaption class="profile col-xs-4 col-sm-4 col-md-3">
+							<div class="moveme row">
+								<h4><?php the_title(); ?></h4>
+								<strong class="ampersand"><?php echo $title; ?></strong>
+								<p><?php the_content(); ?></p>
+							</div><!-- moveme -->
+						</figcaption><!-- profile -->
 
-						<h2><?php the_title(); ?></h2>
-						<h3><?php echo $title; ?></h3>
-						<p><?php the_content(); ?></p>
-					</li>
+					<?php endwhile; ?>
 
-				<?php endwhile; // end of the loop. ?>
-			</ul>
+				<?php else : ?>
+					<h1>These aren't the droids you're looking for.</h1>
+					<p>Move along...</p>
+				<?php endif; ?>
 
+				<?php wp_reset_query(); ?>
 
-
-		<?php else : ?>
-			<h1>These aren't the droids you're looking for.</h1>
-			<p>Move along...</p>
-		<?php endif; ?>
-
-		<?php wp_reset_query(); ?>
-	</div><!-- container -->
-</section><!-- about -->
+		</div><!-- main-bg -->
+	</figure>
+</section><!-- teamback -->
