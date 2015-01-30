dc798b120946bba416bb719ddfa900aaff965712
Skylar Wuebker <skylar@skylarwuebker.com>
2015-01-29 20:25:53 -0500

    footer.php

--- footer.php
+++ footer.php
@@ -8,15 +8,36 @@
  */
 ?>
 
+ <footer>
+    <div class="container">
+    	<div class="row">
+    		<div class="info col-xs-12 col-sm-6 col-md-5 col-md-offset-1">
+             <span><strong>Creativity</strong></span><br><p>Creative Folk is a full service design group specializing in website design and development, print communications, public relations, and social media networking. Serving Akron, Cleveland, and Canton Ohio areas. Advailible for travel worldwide.
+         </div>
+         <div class="info col-xs-12 col-sm-4 col-md-3">
+             <span><strong>How Can We Improve?</strong></span><p> We are always looking to improve and we would love your input. Tell us what you think <a href="mailto:folks@creative-folk.com">here.</a></p>
+         </div>
+     </div><!-- row -->
+     <div class="row">
+         <div class="info col-xs-12 col-sm-6 col-md-5 col-md-offset-1">
+             <span><strong>Navigation</strong></span><p> <a href="#welcome">welcome</a> | <a href="#about">About Us</a> | <a href="#team">Team </a>| <a href="#portfolio">Work </a>| <a href="#contact">Contact </a>| <a href="#">Payment </a>| Careers</p>
+         </div>
+         <div class="info contact col-xs-12 col-sm-4 col-md-3">
+             <span><strong> Contact Us </strong></span><p> 150 E. Exchange St.<br> Akron, OH 44311<br>Phone: 419.953.0244<br>Email: folks@creative-folk.com</p>
+         </div>
+     </div><!-- row -->
+ </div><!-- container -->
+</footer>
+
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="<?php bloginfo('template_directory'); ?>/js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
 
-<script src="<?php bloginfo('template_directory'); ?>/js/vendor/bootstrap.min.js"></script>
-<script src="<?php bloginfo('template_directory'); ?>/js/vendor/jquery.scrollTo.min.js"></script>
-<script src="<?php bloginfo('template_directory'); ?>/js/vendor/jquery.localScroll.min.js"></script>
-<script src="<?php bloginfo('template_directory'); ?>/js/vendor/skrollr.js"></script>
+<script src="js/vendor/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/vendor/jquery.scrollTo.min.js"></script>
+<script type="text/javascript" src="js/vendor/jquery.localScroll.min.js"></script>
+<script type="text/javascript" src="js/vendor/skrollr.js"></script>
 
-<script src="<?php bloginfo('template_directory'); ?>/js/main.js"></script>
-<?php wp_footer(); ?>
+<script src="js/main.js"></script>
 </body>
 </html>
+
