afddda5b309ad15a4de2c219328ca20baff67bef
Skylar Wuebker <skylar@skylarwuebker.com>
2015-01-29 19:56:48 -0500

    Work.php

--- work.php
+++ work.php
@@ -5,46 +5,70 @@
  * @package CreativeFolk
  */
 ?>
+    <section id="portfolio">
+        <div class="portHeader sectionHeader">
+            <h1 class="sr-only">Our Portfolio</h1>
+            <h3 class="visible-md visible-lg">We make awesome stuff</h3>
+        </div>
+        <div class="container-fluid">
+            <?php query_posts('category_name=work'); ?>
+            <?php if (have_posts()) : ?>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <h5>Web</h5>
+                        <div class="row">
+                            <ul class="list-unstyled portfolioList">
+                                <?php while ( have_posts() ) : the_post(); ?>
+                                <?php $blurb = get_field('blurb'); ?>
+                                <li>
+                                    <figure>
+                                        <?php the_post_thumbnail( 'thumbnail', array( 'class' => 'port-img' ) ); ?>
+                                        <figcaption>
+                                            <h4><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h4>
+                                            <p><?php echo $blurb; ?></p>
+                                        </figcaption>
+                                    </figure>
+                                </li>
+                            </ul>
+                        </div>
+                </div><!-- column -->
+                <div class="col-xs-12">
+                    <h5>Print</h5>
+                    <div class="row">
+                        <ul class="list-unstyled portfolioList">
+                            <?php while ( have_posts() ) : the_post(); ?>
+                            <?php $blurb = get_field('blurb'); ?>
+                            <li>
+                                <figure>
+                                    <?php the_post_thumbnail( 'thumbnail', array( 'class' => 'port-img' ) ); ?>
+                                    <figcaption>
+                                        <h4><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h4>
+                                        <p><?php echo $blurb; ?></p>
+                                    </figcaption>
+                                </figure>
+                            </li>
+                        </ul>
+                    </div>
+                </div><!-- column -->
+                <div class="col-xs-12">
+                    <h5>Print</h5>
+                    <div class="row">
+                        <ul class="list-unstyled portfolioList">
+                            <?php while ( have_posts() ) : the_post(); ?>
+                            <?php $blurb = get_field('blurb'); ?>
+                            <li>
+                                <figure>
+                                    <?php the_post_thumbnail( 'thumbnail', array( 'class' => 'port-img' ) ); ?>
+                                    <figcaption>
+                                        <h4><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h4>
+                                        <p><?php echo $blurb; ?></p>
+                                    </figcaption>
+                                </figure>
+                            </li>
+                        </ul>
+                    </div>
+                </div><!-- column -->
+            </div><!-- .row -->
+        </div><!-- .container-fluid -->
+    </section><!-- portfolio -->
 
-<section id="work">
-
-  <figure class="sectionheader">
-      <figcaption>
-          <h1>Our Work</h1>
-      </figcaption>
-  </figure>
-  <div class="container-fluid">
-
-    <?php query_posts('category_name=work'); ?>
-    <?php if (have_posts()) : ?>
-
-      <ul class="list-unstyled row">
-
-        <?php while ( have_posts() ) : the_post(); ?>
-        <?php 
-          $blurb = get_field('blurb'); 
-          ?>
-
-          <li class="col-xs-12 col-sm-6 col-lg-4 work-entry">
-              <figure class="row">
-                  <?php the_post_thumbnail( 'thumbnail', array( 'class' => 'port-img' ) ); ?>
-                  <figcaption class="col-xs-12">
-                      <h4><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h4>
-                      <p><?php echo $blurb; ?></p>
-                  </figcaption>
-              </figure>
-          </li>
-
-
-        <?php endwhile; // end of the loop. ?>
-
-      </ul>
-
-    <?php else : ?>
-      <h1>These aren't the droids you're looking for.</h1>
-      <p>Move along...</p>
-    <?php endif; ?>
-
-  <?php wp_reset_query(); ?>
-  </div><!-- container -->
-</section><!-- work -->
