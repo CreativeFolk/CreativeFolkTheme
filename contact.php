897ca2b57a919d679c155134c4890f2cd1412f3c
Skylar Wuebker <skylar@skylarwuebker.com>
2015-01-29 20:18:27 -0500

    contact.php

--- contact.php
+++ contact.php
@@ -9,7 +9,9 @@
 
   <figure class="sectionheader">
       <figcaption>
-          <h1>Contact</h1>
+          <h1 class="sr-only">Contact us</h1>
+            <h3>Based out of the home of the Akron Zips</h3>
+            <p class="col-sm-10 col-md-8 col-lg-6 col-sm-offset-1 col-md-offset-2 col-lg-offset-3">Our base of operations is Akron, Ohio. If you're nearby we'd love to meet up and talk about stuff. If you're not nearby, we're pretty tech savvy people and we'll virtually meet up with you any way that's convienent.</p>
       </figcaption>
   </figure>
   <div class="container">
