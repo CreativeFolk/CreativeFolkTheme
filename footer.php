3768231081401a1e7efc9dbd0ab05f4e56c5c264
Skylar Wuebker <skylar@skylarwuebker.com>
2015-01-29 20:31:58 -0500

    like this?

--- footer.php
+++ footer.php
@@ -33,9 +33,9 @@
 <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
 
 <script src="js/vendor/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/vendor/jquery.scrollTo.min.js"></script>
-<script type="text/javascript" src="js/vendor/jquery.localScroll.min.js"></script>
-<script type="text/javascript" src="js/vendor/skrollr.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>js/vendor/jquery.scrollTo.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>js/vendor/jquery.localScroll.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>js/vendor/skrollr.js"></script>
 
 <script src="js/main.js"></script>
 </body>
