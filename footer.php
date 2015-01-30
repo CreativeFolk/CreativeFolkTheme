2f4d8c1a0f81372bb05c2ff3e19e4c74a98b4e7c
Skylar Wuebker <skylar@skylarwuebker.com>
2015-01-29 20:35:45 -0500

    fixxes

--- footer.php
+++ footer.php
@@ -30,12 +30,12 @@
 </footer>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
+<script>window.jQuery || document.write('<script src="<?php bloginfo('template_directory'); ?>js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
 
-<script src="js/vendor/bootstrap.min.js"></script>
-<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>js/vendor/jquery.scrollTo.min.js"></script>
-<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>js/vendor/jquery.localScroll.min.js"></script>
-<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>js/vendor/skrollr.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>js/vendor/bootstrap.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>js/vendor/jquery.scrollTo.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>js/vendor/jquery.localScroll.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>js/vendor/skrollr.js"></script>
 
 <script src="js/main.js"></script>
 </body>
